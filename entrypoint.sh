ab1e72a783685bf44ff6ebca6f3e90ebfb79477a
Christian <github@christianwolf.email>
2020-02-24 17:22:18 +0100

    Added sleep loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,21 @@
 
 echo Hello world
 
+echo "Waiting a few seconds for startup"
+
+for i in `seq 1 100`
+do
+  mysql --host=mariadb --port=3307 --protocol=tcp -u partkeepr -ppartkeepr partkeepr_test <<- EOF
+    SELECT version();
+  EOF
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  
+  echo "Sleeping..."
+  sleep 1
+done
+
 mysql --host=mariadb --port=3307 --protocol=tcp -u partkeepr -ppartkeepr partkeepr_test << EOF
 SHOW DATABASES;
 EOF
